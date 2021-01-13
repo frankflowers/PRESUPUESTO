3ad422c1e1d33f87b71f72f5e7877b78700b99be
frankflowers <franciscojair.flores.hernandez@gmail.com>
2021-01-13 16:25:53 -0600

    Subir

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+//App.js
+import React,{useState,useEffect, Fragment} from 'react';
+import  Pregunta  from './components/Pregunta';
+import  Formulario  from './components/Formulario';
+import Listado from "./components/Listado";
+import ControlPresupuesto from "./components/ControlPresupuesto";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//definir el state
+const [presupuesto,guardarPresupuesto]= useState(0);
+const [restante, guardarRestante] = useState(0);
+const [mostrarpregunta,actualizarPregunta]= useState(true);
+const [gastos,guardarGastos]= useState([]);
+const [gasto,guardarGasto]= useState([]);
+const [creargasto, guardarCrearGasto] = useState(false);
+// usEffect que actualice le restante
+useEffect(() => {
+  if(creargasto){ 
+  guardarGastos([
+    ...gastos,
+    gasto
+  ]);
+//resta del presupuesto actual
+const presupuestoRestante = restante - gasto.cantidad;
+guardarRestante(presupuestoRestante);
+  //resetear false
+  guardarCrearGasto(false);
 }
+}, [gasto,creargasto,gastos,restante])
+
+
+
+
+  return (
+    <Fragment>
+  <div className= 'container'>
+    <header>
+      <h1>Gasto semanal</h1>
+    
+    <div className= 'contenido-principal contenido'>
+    {mostrarpregunta ? 
+    (
+    <Pregunta 
+      guardarPresupuesto={guardarPresupuesto}
+      guardarRestante={guardarRestante}
+      actualizarPregunta={actualizarPregunta}
+      />
+    ) :
+    (
+ <div className="row">
+  <div className= "one-half column">
+      <Formulario
+      guardarGasto={guardarGasto}
+      guardarCrearGasto={guardarCrearGasto}
+      
+      
+      />
+  </div>
+  
+  <div className="one-half column">
+    <Listado
+    gastos={gastos}
+    />
+    <ControlPresupuesto
+    presupuesto={presupuesto}
+    restante= {restante}
+    />
+    </div>
+  </div> 
+  
+        
+        )
+        }
+
+    </div>
+     </header>
+  </div>
+  </Fragment>
+  );
+    }
 
 export default App;
---
+++ src/components/Listado.js
@@ -0,0 +1,31 @@
+
+//Listado
+import React from 'react';
+import Gasto from "./Gasto";
+import PropTypes from 'prop-types';
+const Listado = ({ gastos }) => {
+    console.log(gastos)
+    return (
+  
+      <div className="gastos-realizados">
+  
+        <h2>Listado</h2>
+  
+        {gastos.map((gasto) => {
+            
+          return <Gasto 
+          key={gasto.id} gasto={gasto} />;
+  
+        })}
+  
+      </div>
+  
+    );
+  
+  };
+  Listado.propTypes={
+gastos: PropTypes.array.isRequired
+
+  }
+  
+export default Listado;
